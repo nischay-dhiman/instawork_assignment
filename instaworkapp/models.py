73863e63013f0b517054b9fb43600b7d9214792f
Nischay Dhiman <nischay.dhiman@gmail.com>
2022-07-11 11:20:36 -0400

    phone validation added

--- instaworkapp/models.py
+++ instaworkapp/models.py
@@ -14,7 +14,7 @@ class TeamMember(models.Model):
     assigned_role = models.IntegerField(choices=ROLE_CHOICES, default=0)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                                  message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=17, null=False, blank=False)
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, null=True, blank=False)
 
     def role_text(self):
         if self.assigned_role == 1:
