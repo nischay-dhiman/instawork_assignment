7ba461afa6e6df69bd3c0d34abac2e4e8595b8f8
Nischay Dhiman <nischay.dhiman@gmail.com>
2022-07-12 00:13:58 -0400

    fixing static images path

--- instawork_assignment/settings.py
+++ instawork_assignment/settings.py
@@ -117,15 +117,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-SITE_ROOT = ''
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (
-    os.path.join(SITE_ROOT, '..', 'static'),
-)
+STATIC_ROOT = '/home/instawork_assignment/static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
